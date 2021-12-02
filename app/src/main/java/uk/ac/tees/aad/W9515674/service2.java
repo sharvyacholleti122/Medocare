82cdb8939c8ab18f4f094fea2c90a22a4529a0b1
sharvyacholleti122 <sharvyacholleti122@gmail.com>
2021-12-02 20:03:20 +0530

    added posting

---
+++ app/src/main/java/uk/ac/tees/aad/W9515674/service2.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.aad.W9515674;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class service2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service2);
+    }
+}
